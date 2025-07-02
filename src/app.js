6535fbb76badcfec289bfc1a040812e6fa86bc15
vaishnavirajj <vaishnaviraj950@gmail.com>
2025-07-03 01:27:49 +0530

    Initail commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log("Starting a new project");
+const express = require("express");
+const app =express();
+
+//request handler
+app.use((req , res) => {
+    res.send("hello from the server!");
+})
+
+app.use("/home" ,(req,res)=> {
+    res.send("You are at Home route server");
+    
+
+})
+app.listen(3000 , () => {
+    console.log("Server is running on port 3000");
+     
+});
---
+++ src/homework.md
@@ -0,0 +1,11 @@
+-Create a repository
+-Initialize the repository
+-node_modules , package.json,package-lock.json
+-Install express
+-Create a server
+-Listen to port 7777
+-Write request handlers for /test , /hello
+-Install nodemon and update scripts inside package.json
+-what are dependencies
+-what is the use of "-g" while npm install
+-difference between caret and tilda (^ vs ~)
