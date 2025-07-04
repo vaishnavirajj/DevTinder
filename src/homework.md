e063f67de31b4c551af149e0a0a54f1409cf199a
vaishnavirajj <vaishnaviraj950@gmail.com>
2025-07-04 07:17:34 +0530

    wrote-auth-middlewares

--- src/homework.md
+++ src/homework.md
@@ -31,3 +31,8 @@
 -next()
 -next function and errors along with res.send()
 -app.use("/route" ,rH ,[rH2,rH3],rH4,rH5)
+-What is a Middleware ?why do we need it
+-How express JS basically handles requests behind the scenes
+-Difference between app.use() and app.all()
+-Write dummy auth middleware for admin
+-Write a dummy auth middleware for all user routes , except /user/login
---
+++ src/middlewares/auth.js
@@ -0,0 +1,30 @@
+const adminAuth = (req,res,next) => {
+    console.log("Admin auth is getting checked!!");
+    
+    const token ="xyz";
+    const isAdminAuthorized =token ==="xyz"
+    if (!isAdminAuthorized) {
+        res.status(401).send("Unauthorized request");
+    }
+    else {
+        next();
+    }
+    
+};
+
+const userAuth =(req , res, next) => {
+    console.log("User Auth is getting checked!!");
+    
+    const token ="xyz";
+    const isAdminAuthorized =token ==="xyz";
+    if(isAdminAuthorized) {
+        res.status(401).send("Unauthorized request");
+    }
+    else {
+        next();
+    }
+}
+
+module.exports = {
+    adminAuth,userAuth,
+};
